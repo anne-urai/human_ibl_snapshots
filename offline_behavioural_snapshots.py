5c03fd2e0602f27b25907b2f59e2f0e8a592a64f
phijoh <pajohnson@student.unimelb.edu.au>
2024-04-23 13:37:02 +0200

    plot all trials

--- offline_behavioural_snapshots.py
+++ offline_behavioural_snapshots.py
@@ -93,7 +93,7 @@ for file_name, file_content in downloaded_files:
     try:
         fig_name = file_name.split('/')[-1].split('_202')
         fig_name = os.path.join(os.getcwd(), 'offline_snapshot_figures', 
-                                '202'+ fig_name[1].split('.csv')[0] + '_' +  fig_name[0] + '.png')
+                                '202'+ fig_name[1].split('.csv')[0] + '_' +  fig_name[0].replace('data\\','') + '.png')
         
         if os.path.exists(fig_name):
             print("skipping ", file_name, ", already exists")
@@ -109,7 +109,14 @@ for file_name, file_content in downloaded_files:
                 data = data.merge(response_mappings, on=['eccentricity', 'correct'], how='left') # right response = 1, left response = 0
             else: # do this if keyboard responses
                 data['response'] = data['key_resp.keys'].map({'x': 1, 'm': 0}, na_action=None)
+
+            # drop practice trials
+            session_start_ind = np.max(np.argwhere(data['session_start'].notnull()))
+            data = data.iloc[session_start_ind:]
             
+            # add new column that counts all trials
+            data['trials.allN'] = data.reset_index().index
+
             # ============================= %
             # from https://github.com/int-brain-lab/IBL-pipeline/blob/master/prelim_analyses/behavioral_snapshots/behavior_plots.py
             # https://github.com/int-brain-lab/IBL-pipeline/blob/7da7faf40796205f4d699b3b6d14d3bf08e81d4b/prelim_analyses/behavioral_snapshots/behavioral_snapshot.py
@@ -122,7 +129,7 @@ for file_name, file_content in downloaded_files:
           
             # 2. chronometric
             sns.lineplot(data=data, ax=ax[1],
-                         x='signed_contrast', y='reaction_time', err_style="bars", 
+                         x='signed_contrast', y='response_time', err_style="bars", 
                          linewidth=1, estimator=np.median, 
                          mew=0.5,
                          marker='o', errorbar=('ci',68), color='black')
@@ -130,16 +137,21 @@ for file_name, file_content in downloaded_files:
     
             # 4. time on task
             sns.scatterplot(data=data, ax=ax[2], 
-                            x='trials.thisN', y='reaction_time', 
+                            x='trials.allN', y='response_time', 
                             style='correct', hue='correct',
                             palette={1:"#009E73", 0:"#D55E00"}, 
                             markers={1:'o', 0:'X'}, s=10, edgecolors='face',
                             alpha=.5, legend=False)
             
             # running median overlaid
-            sns.lineplot(data=data[['trials.thisN', 'reaction_time']].rolling(10).median(), 
+            sns.lineplot(data=data[['trials.allN', 'response_time']].rolling(10).median(), 
                          ax=ax[2],
-                         x='trials.thisN', y='reaction_time', color='black', errorbar=None, )
+                         x='trials.allN', y='response_time', color='black', errorbar=None, 
+                         label='response time')
+            sns.lineplot(data=data[['trials.allN', 'reaction_time']].rolling(10).median(), 
+                         ax=ax[2],
+                         x='trials.allN', y='reaction_time', color='black', alpha=0.2, errorbar=None, 
+                         label='reaction time')
             ax[2].set(xlabel="Trial number", ylabel="RT (ms)",) #ylim=[0.1, 10])
             ax[2].set_yscale("log")
             ax[2].yaxis.set_major_formatter(mpl.ticker.FuncFormatter(lambda y,pos:
