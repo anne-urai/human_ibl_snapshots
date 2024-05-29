3b3b3fa1e3b6acced5c1e3ae35ce40763ec0b959
Anne Urai <a.e.urai@fsw.leidenuniv.nl>
2024-05-29 16:43:22 +0200

    Anne's rerun, still need to include biased blocks

--- offline_behavioural_snapshots.py
+++ offline_behavioural_snapshots.py
@@ -3,20 +3,27 @@
 Created on March 26 2024
 @author: cami-uche-enwe
 Leiden University
+
+requirements: ibllib https://int-brain-lab.github.io/iblenv/notebooks_external/data_download.html
+
 """
 #%% =============================== #
 # import packages
 # ================================= #
+
+import os
+from io import StringIO
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns 
 sns.set_style('darkgrid')
-import brainbox.behavior.pyschofit as psy
 
-import os
-from io import StringIO
+try:
+    import psychofit as psy # https://int-brain-lab.github.io/iblenv/_autosummary/brainbox.behavior.pyschofit.html, has moved to its own package instead of brainbox
+except:
+    import brainbox.behavior.psychofit as psy
 
 #%% =============================== #
 # get files and file contents
@@ -35,7 +42,7 @@ def get_files_from_folder(folder_path):
         with open(csv_path, 'r') as file:
             file_content = file.read()
             downloaded_files.append((csv_path, file_content))
-    print(downloaded_files)
+    # print(downloaded_files)
     return downloaded_files
 
 # Specify the path to the folder containing your CSV files
@@ -43,10 +50,10 @@ folder_path = "./data"
 # Extract file names and contents from the specified folder
 downloaded_files = get_files_from_folder(folder_path);
 
-
 #%% =============================== #
 # plotting function
 # ================================= #
+
 def plot_psychometric(df, **kwargs):
     
     if 'ax' in kwargs.keys():
@@ -105,6 +112,7 @@ for file_name, file_content in downloaded_files:
             
             # recode some things
             if 'mouse.x' in data.keys(): # do this if mouse responses
+                # this code merges the columns that are now spread over trials_1, trials_2, etc
                 response_mappings = pd.DataFrame({'eccentricity':[15,15,-15,-15], 'correct':[1,0,1,0], 'response':[1,0,0,1]})
                 data = data.merge(response_mappings, on=['eccentricity', 'correct'], how='left') # right response = 1, left response = 0
             else: # do this if keyboard responses
@@ -128,7 +136,8 @@ for file_name, file_content in downloaded_files:
             
             # 1. psychometric
             plot_psychometric(data, ax=ax[0])
-            ax[0].set(xlabel='Signed contrast', ylabel='Choice (fraction)')
+            ax[0].set(xlabel='Signed contrast', ylabel='Choice (fraction)',
+                      ylim=[-0.05, 1.05])
           
             # 2. chronometric
             sns.lineplot(data=data, ax=ax[1],
@@ -136,7 +145,12 @@ for file_name, file_content in downloaded_files:
                          linewidth=1, estimator=np.median, 
                          mew=0.5,
                          marker='o', errorbar=('ci',68), color='black')
-            ax[1].set(xlabel='Signed contrast', ylabel='RT (ms)',) #ylim=[0,2])
+            sns.lineplot(data=data, ax=ax[1],
+                         x='signed_contrast', y='reaction_time', err_style="bars", 
+                         linewidth=1, estimator=np.median, 
+                         mew=0.5,
+                         marker='o', errorbar=('ci',68), color='grey')
+            ax[1].set(xlabel='Signed contrast', ylabel='Reaction time (black) / movement initiation (grey) (ms)', ylim=[0, 1600])
     
             # 3. time on task
             sns.scatterplot(data=data, ax=ax[2], 
@@ -149,20 +163,18 @@ for file_name, file_content in downloaded_files:
             # running median overlaid
             sns.lineplot(data=data[['trials.allN', 'response_time']].rolling(10).median(), 
                          ax=ax[2],
-                         x='trials.allN', y='response_time', color='black', errorbar=None, 
-                         label='median response time')
+                         x='trials.allN', y='response_time', color='black', errorbar=None)
             sns.lineplot(data=data[['trials.allN', 'reaction_time']].rolling(10).median(), 
                          ax=ax[2],
-                         x='trials.allN', y='reaction_time', color='black', alpha=0.2, errorbar=None, 
-                         label='median movement initiation time')
+                         x='trials.allN', y='reaction_time', color='grey', errorbar=None)
             if block_breaks: # add lines to mark breaks
                 [plt.axvline(x, color='blue', alpha=0.2, linestyle='--') for x in np.arange(100,data['trials.allN'].iloc[-1],step=100)]
-            ax[2].set(xlabel="Trial number", ylabel="RT (ms)",) #ylim=[0.1, 10])
+            ax[2].set(xlabel="Trial number", ylabel="RT (ms)", ylim=[10, 10000])
             ax[2].set_yscale("log")
             ax[2].yaxis.set_major_formatter(mpl.ticker.FuncFormatter(lambda y,pos:
                 ('{{:.{:1d}f}}'.format(int(np.maximum(-np.log10(y),0)))).format(y)))
     
-            fig.suptitle(fig_name.split('\\')[-1])
+            fig.suptitle(os.path.split(fig_name)[-1])
             fig.tight_layout(rect=[0, 0.03, 1, 0.95])
             sns.despine(trim=True)
             fig.savefig(fig_name)
