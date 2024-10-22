6a8687e3b9b46625556d026e04a428b9e6c2a6aa
phijoh <pajohnson@student.unimelb.edu.au>
2024-10-22 17:53:15 +0200

    add my user details

--- snapshot_all.py
+++ snapshot_all.py
@@ -31,6 +31,8 @@ import utils
 usr = os.path.expanduser("~")
 if usr == '/Users/uraiae': # mounted using mountainduck
     folder_path = '/Volumes/macOS/Users/uraiae/VISUAL-DECISIONS.localized/subjects/'
+elif usr = 'C:\\Users\\Philippa':
+    folder_path = 'D:\winshare\workgroups\FSW\VISUAL-DECISIONS\subjects'
 else: # for local data
     folder_path = "./data"
 
